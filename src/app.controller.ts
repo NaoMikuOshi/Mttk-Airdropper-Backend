ee602918adc43fe559a57ebdfec7a96eeeeb24c6
Nao KM <NaoMikuOshi@outlook.com>
2020-06-28 11:56:11 +0800

    refactor: disable unused function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,16 +5,19 @@ import { VirtualAccountService } from './virtual-account/virtual-account.service
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly virtualAccountService: VirtualAccountService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly virtualAccountService: VirtualAccountService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get('/ttt')
-  async getAccess(@Headers('x-access-token') accessToken: string): Promise<any> {
-    const access_token = await this.virtualAccountService.getAccessToken();
-    console.log(access_token)
-    return access_token;
-  }
+  // @Get('/ttt')
+  // async getAccess(@Headers('x-access-token') accessToken: string): Promise<any> {
+  //   const access_token = await this.virtualAccountService.getAccessToken();
+  //   console.log(access_token)
+  //   return access_token;
+  // }
 }
