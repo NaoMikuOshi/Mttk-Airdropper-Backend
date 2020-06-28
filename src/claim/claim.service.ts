3e38715d00dbf8087997b6d4e06212b6c30834ac
Nao KM <NaoMikuOshi@outlook.com>
2020-06-28 16:09:41 +0800

    fix: add typing

--- src/claim/claim.service.ts
+++ src/claim/claim.service.ts
@@ -1,32 +1,33 @@
-import { HttpService, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
-import { ClaimLog } from '../entities/ClaimLog.entity'
+import { HttpService, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { ClaimLog } from '../entities/ClaimLog.entity';
 import {
-    Crud,
-    CrudController,
-    Override,
-    CrudRequest,
-    ParsedRequest,
-    ParsedBody,
-    CrudAuth,
+  Crud,
+  CrudController,
+  Override,
+  CrudRequest,
+  ParsedRequest,
+  ParsedBody,
+  CrudAuth,
 } from '@nestjsx/crud';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ClaimService extends TypeOrmCrudService<ClaimLog> {
-    constructor(
-        @InjectRepository(ClaimLog) repo,
-        private readonly httpService: HttpService
-    ) {
-        super(repo);
-    }
-    async createClaim(dto) {
-        let item = new ClaimLog();
-        item.uid = dto.uid;
-        item.hash_tag = dto.hash_tag;
-        item.amount = dto.amount;
-        item.token_id = dto.token_id;
-        item.tx_hash = dto.tx_hash;
-        return this.repo.save(item)
-    }
+  constructor(
+    @InjectRepository(ClaimLog) repo: Repository<ClaimLog>,
+    private readonly httpService: HttpService,
+  ) {
+    super(repo);
+  }
+  async createClaim(dto) {
+    let item = new ClaimLog();
+    item.uid = dto.uid;
+    item.hash_tag = dto.hash_tag;
+    item.amount = dto.amount;
+    item.token_id = dto.token_id;
+    item.tx_hash = dto.tx_hash;
+    return this.repo.save(item);
+  }
 }
