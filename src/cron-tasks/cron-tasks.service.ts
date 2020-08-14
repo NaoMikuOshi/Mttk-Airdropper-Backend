a4f7075a4c439ca013a8f0492ec5510136e90109
Nao KM <NaoMikuOshi@outlook.com>
2020-08-14 14:56:11 +0800

    feat: handle finished events

--- src/cron-tasks/cron-tasks.service.ts
+++ src/cron-tasks/cron-tasks.service.ts
@@ -5,6 +5,7 @@ import { ClaimLog } from 'src/entities/ClaimLog.entity';
 import { Repository } from 'typeorm';
 import { AirdropService } from 'src/airdrop/airdrop.service';
 import { AuthService } from 'src/auth/auth.service';
+import { AirdropEvent } from 'src/entities/AirdropEvent.entity';
 
 @Injectable()
 export class CronTasksService {
@@ -12,6 +13,8 @@ export class CronTasksService {
   constructor(
     @InjectRepository(ClaimLog)
     private readonly claimRepo: Repository<ClaimLog>,
+    @InjectRepository(AirdropEvent)
+    private readonly aeRepo: Repository<AirdropEvent>,
     private readonly airdropService: AirdropService,
     private readonly authService: AuthService,
   ) {}
@@ -61,6 +64,24 @@ export class CronTasksService {
     this.claimRepo.save(notSentLog);
     this.logger.log(
       `Claim transaction ${notSentLog.id} is now sent, txHash is ${notSentLog.tx_hash}`,
+    );
+  }
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async updateFinished() {
+    const currentActiveEvents = await this.aeRepo.find({ status: 'active' });
+    const finishedEvents = currentActiveEvents.filter(
+      (event) => event.quantity == event.claimed,
+    );
+    if (finishedEvents.length === 0) return;
+    const ableToUpdate = finishedEvents.map((e) => ({
+      ...e,
+      status: 'finished' as 'finished',
+    }));
+    await this.aeRepo.save(ableToUpdate);
+    this.logger.log(
+      'update status as finished for events id: ' +
+        finishedEvents.map((e) => e.id).join(),
     );
   }
 }
